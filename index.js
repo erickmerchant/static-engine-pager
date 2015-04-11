555f43bc1921ef91c93803ab57f0572205a121c1
Erick Merchant <erickmerchant@gmail.com>
2015-04-11 02:40:22 -0700

    Adopt JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 module.exports = function (pages, done) {
+  pages.forEach(function (val, key) {
+    if (key - 1 > -1) {
+      pages[key].next = pages[key - 1]
+    }
 
-    pages.forEach(function (val, key) {
+    if (key + 1 < pages.length) {
+      pages[key].previous = pages[key + 1]
+    }
+  })
 
-        if (key - 1 > -1) {
-
-            pages[key].next = pages[key - 1];
-        }
-
-        if (key + 1 < pages.length) {
-
-            pages[key].previous = pages[key + 1];
-        }
-    });
-
-    done(null, pages);
-};
+  done(null, pages)
+}
--- test/test.js
+++ test/test.js
@@ -1,49 +1,58 @@
-var assert = require('assert');
-var pager = require('../index.js');
+var assert = require('assert')
+var describe = require('mocha').describe
+var it = require('mocha').it
+var pager = require('../index.js')
 
-describe('plugin', function(){
+describe('plugin', function () {
+  it('it should work with zero pages', function (done) {
+    pager([], function (err, pages) {
+      if (err) {
+        throw err
+      }
 
-    it('it should work with zero pages', function(done){
+      assert.deepEqual(pages, [])
 
-        pager([], function(err, pages){
+      done()
+    })
+  })
 
-            assert.deepEqual(pages, []);
+  it('it should work with one page', function (done) {
+    pager([{title: 'one'}], function (err, pages) {
+      if (err) {
+        throw err
+      }
 
-            done();
-        });
-    });
+      assert.deepEqual(pages, [{title: 'one'}])
 
-    it('it should work with one page', function(done){
+      done()
+    })
+  })
 
-        pager([{title: 'one'}], function(err, pages){
+  it('it should work with two pages', function (done) {
+    pager([{title: 'one'}, {title: 'two'}], function (err, pages) {
+      if (err) {
+        throw err
+      }
 
-            assert.deepEqual(pages, [{title: 'one'}]);
+      assert.deepEqual(pages[0].previous, pages[1])
+      assert.deepEqual(pages[1].next, pages[0])
 
-            done();
-        });
-    });
+      done()
+    })
+  })
 
-    it('it should work with two pages', function(done){
+  it('it should work with more pages', function (done) {
+    pager([{title: 'one'}, {title: 'two'}, {title: 'three'}], function (err, pages) {
+      if (err) {
+        throw err
+      }
 
-        pager([{title: 'one'}, {title: 'two'}], function(err, pages){
+      assert.deepEqual(pages[0].previous, pages[1])
+      assert.deepEqual(pages[1].previous, pages[2])
+      assert.deepEqual(pages[1].next, pages[0])
+      assert.deepEqual(pages[2].next, pages[1])
 
-            assert.deepEqual(pages[0].previous, pages[1]);
-            assert.deepEqual(pages[1].next, pages[0]);
-
-            done();
-        });
-    });
-
-    it('it should work with more pages', function(done){
-
-        pager([{title: 'one'}, {title: 'two'}, {title: 'three'}], function(err, pages){
-
-            assert.deepEqual(pages[0].previous, pages[1]);
-            assert.deepEqual(pages[1].previous, pages[2]);
-            assert.deepEqual(pages[1].next, pages[0]);
-            assert.deepEqual(pages[2].next, pages[1]);
-
-            done();
-        });
-    });
-});
+      done()
+    })
+  })
+})
